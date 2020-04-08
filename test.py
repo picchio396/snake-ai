a6a4b207bfd79b5a4135787997117f3c9f7a8830
blackbox <nada>
2020-04-08 14:05:05 -0400

    very dumb ai

--- test.py
+++ test.py
@@ -13,30 +13,30 @@ import random
 import numpy as np
 import constants
 
-MAX_EPOCHS = 5000
+MAX_EPOCHS = 150
 
-## TODO
-# figure out how to stop
-# figure out how to save
-
-# q_table = np.zeros([])
 
 # All 0 < x < 1 
 # alpha: learning rate (the extent to which our Q-values are being updated in every iteration)
 # gamma: discount factor (how much importance we want to give to future rewards)
 # epsilon: exploration faction (exploration (choosing alpha random action) vs exploitation (choosing actions based on already learned Q-values))
-alpha = 0.1 #0.1
+alpha = 0.01 #0.1
 gamma = 0.6 #0.6
-epsilon = 0.1 #0.1
+epsilon = 0.1
+
+# # epsilon = 0.1
+epsilon = 1
+epsilon_rate = 0.99
+min_epsilon = 0.3
 
 env = gym.make('snake-v0')
 
-try:
-    print('Loading...')
-    q_table = np.load('q_table.npy')
-except:
-    print("Initializing...")
-    q_table = np.zeros([int(constants.MAX_WIDTH*2), int(constants.SCREEN_HEIGHT*2), int(constants.MAX_WIDTH*2), int(constants.MAX_HEIGHT*2), 4])
+# try:
+#     print('Loading...')
+#     q_table = np.load('q_table.npy')
+# except:
+#     print("Initializing...")
+q_table = np.zeros([int(constants.MAX_WIDTH*2), int(constants.SCREEN_HEIGHT*2), int(constants.MAX_WIDTH*2), int(constants.MAX_HEIGHT*2), 4])
 
 # For plotting metrics
 all_epochs = []
@@ -71,6 +71,9 @@ for i in range(0, MAX_EPOCHS):
         state = next_state
         epochs += 1
 
+        # Updating epsilon value
+        epsilon = epsilon * epsilon_rate if epsilon > min_epsilon else min_epsilon
+
     
     if score > max_score:
         max_score = score
@@ -78,8 +81,8 @@ for i in range(0, MAX_EPOCHS):
     if i % 100 == 0:
         print(f"Episode: {i}")
 
-print('Saving...')
-np.save('q_table.npy', q_table)
+# print('Saving...')
+# np.save('q_table.npy', q_table)
 
 print("Max score: " + str(max_score))
 print("Training finished.\n")
